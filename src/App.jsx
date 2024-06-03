65e080d89b1ed2347682e5fc376425948cb4c888
Konrad Izdebski <57152167+Thymoos@users.noreply.github.com>
2024-06-03 11:13:45 +0200

    Created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Heroarea from "./components/hero/Heroarea.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Heroarea />
     </>
   );
