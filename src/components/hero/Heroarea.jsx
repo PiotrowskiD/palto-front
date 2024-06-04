5c1b396490376d7457e19c1c7e0744a8bd09ed0c
Konrad Izdebski <57152167+Thymoos@users.noreply.github.com>
2024-06-04 11:24:43 +0200

    Styled heroarea down to mobile

--- src/components/hero/Heroarea.jsx
+++ src/components/hero/Heroarea.jsx
@@ -3,18 +3,24 @@ import React from "react";
 export default function Heroarea() {
   return (
     <div className={"heroarea-wrapper flex items-end"}>
-      <div className="text flex flex-col justify-center items-center mb-[80px]">
+      <div className="text flex flex-col justify-center items-center mb-[80px] 996px:px-[16px]">
         <h1
           className={
-            "font-extrabold text-[100px] text-center leading-[95px] tracking-[-3px] uppercase text-[#FEFDFB]"
+            "font-[800] text-[100px] text-center leading-[95px] tracking-[-3px] uppercase text-[#FEFDFB] 996px:text-[60px] 996px:leading-[60px] 600px:text-[36px] 600px:leading-[36px]"
           }
         >
           We make AI freelancing easy and stress-free.
         </h1>
-        <p className={"font-500 text-[32px] text-[#FEFDFB] mt-[16px]"}>
+        <p
+          className={
+            "font-[500] text-[32px] text-[#FEFDFB] mt-[16px] 996px:text-center 600px:text-[24px] 600px:leading-[31.2px]"
+          }
+        >
           Focus on getting the work done and start living a balanced life.
         </p>
-        <button className="btn-primary mt-[40px]">Start living!</button>
+        <button className="btn-primary mt-[40px] 600px:mt-[48px]">
+          Start living!
+        </button>
       </div>
     </div>
   );
