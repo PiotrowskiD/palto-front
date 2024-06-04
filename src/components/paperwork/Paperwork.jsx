1a5f66264f338ff273b8355056228b420b982e9d
Konrad Izdebski <57152167+Thymoos@users.noreply.github.com>
2024-06-04 15:33:11 +0200

    Styled paperwork section down to mobile

--- src/components/paperwork/Paperwork.jsx
+++ src/components/paperwork/Paperwork.jsx
@@ -4,13 +4,13 @@ export default function Paperwork() {
   return (
     <section
       className={
-        "paperwork-section bg-[#DAB2ED] flex flex-col justify-center items-center py-[100px]"
+        "paperwork-section bg-[#DAB2ED] flex flex-col justify-center items-center py-[100px] px-[40px] 600px:py-[60px] 600px:px-[16px]"
       }
     >
-      <h1 className="font-extrabold text-[80px] leading-[80px] uppercase">
+      <h1 className="font-[800] text-[80px] leading-[80px] uppercase text-center 600px:text-[32px] 600px:leading-[35.2px]">
         Paperwork and formalities
       </h1>
-      <p className="mt-[20px] font-500 text-[24px] leading-[31.2px] text-center max-w-[900px]">
+      <p className="mt-[20px] font-500 text-[24px] leading-[31.2px] text-center max-w-[900px] 600px:txt-[20px] 600px:leading-[26px] 600px:mt-[16px]">
         We do all that boring stuff, so you don't have to. Life is better
         without stressing about taxes and legal issues. We'll take it off your
         shoulders by organizing everything you need to focus on your work
