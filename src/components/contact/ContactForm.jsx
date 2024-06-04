bab0f2a064026dde65ce6fdcd7dcf27a991585b5
Konrad Izdebski <57152167+Thymoos@users.noreply.github.com>
2024-06-04 10:33:54 +0200

    Made contact checkbox work

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import { useState } from "react";
 
 export default function ContactForm() {
+  const [isConsentChecked, setIsConsentChecked] = useState(false);
+
+  const handleCheckboxChange = (e) => {
+    setIsConsentChecked(e.target.checked);
+  };
+
   return (
     <div className={"contact-form flex flex-col gap-[20px] mt-[80px]"}>
       <input type="text" placeholder={"Name"} />
       <input type="text" placeholder={"Email"} />
       <input type="text" placeholder={"Telephone"} />
       <input type="text" placeholder={"Message"} />
-      <div className="checkbox-wrapper flex gap-[16px] items-center">
-        {/*<div className="checkbox-container">*/}
-        {/*  <input type="checkbox" id="privacy-policy" />*/}
-        {/*</div>*/}
-        <p>I agree to the Privacy Policy.</p>
+      <div className="checkbox-wrapper flex gap-[16px] items-start">
+        <label className="custom-checkbox">
+          <input
+            checked={isConsentChecked}
+            onChange={handleCheckboxChange}
+            type="checkbox"
+          />
+          <span className="checkmark"></span>
+        </label>
+        <p onClick={() => setIsConsentChecked(!isConsentChecked)}>
+          I agree to the Privacy Policy.
+        </p>
       </div>
       <button className="btn-primary self-start mt-[40px]">Send message</button>
     </div>
