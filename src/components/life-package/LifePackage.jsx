8089ca6ac1a689ab1e0eae41f6b86b4d6f9115cb
Konrad Izdebski <57152167+Thymoos@users.noreply.github.com>
2024-06-04 13:56:02 +0200

    Changed life package section margin top for mobile

--- src/components/life-package/LifePackage.jsx
+++ src/components/life-package/LifePackage.jsx
@@ -6,7 +6,7 @@ export default function LifePackage() {
   return (
     <section
       className={
-        "life-package h-[100vh] bg-[#DAB2ED] mt-[120px] flex justify-center items-center px-[40px] 600px:h-auto 600px:py-[116px] 600px:px-[16px]"
+        "life-package h-[100vh] bg-[#DAB2ED] mt-[120px] flex justify-center items-center px-[40px] 600px:h-auto 600px:py-[116px] 600px:px-[16px] 600px:mt-[60px]"
       }
     >
       <h1>
