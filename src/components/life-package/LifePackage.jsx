971715b0565e18d8af49801a23c91ff32c6b292c
Konrad Izdebski <57152167+Thymoos@users.noreply.github.com>
2024-06-04 13:53:07 +0200

    Styled life package section down to mobile

--- src/components/life-package/LifePackage.jsx
+++ src/components/life-package/LifePackage.jsx
@@ -6,7 +6,7 @@ export default function LifePackage() {
   return (
     <section
       className={
-        "life-package h-[100vh] bg-[#DAB2ED] mt-[120px] flex justify-center items-center"
+        "life-package h-[100vh] bg-[#DAB2ED] mt-[120px] flex justify-center items-center px-[40px] 600px:h-auto 600px:py-[116px] 600px:px-[16px]"
       }
     >
       <h1>
