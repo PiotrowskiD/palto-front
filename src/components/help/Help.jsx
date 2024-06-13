a88065b9456843f4920d1d1b610301129b48e333
Konrad Izdebski <57152167+Thymoos@users.noreply.github.com>
2024-06-13 15:05:26 +0200

    Fixed help section polish line height

--- src/components/help/Help.jsx
+++ src/components/help/Help.jsx
@@ -1,11 +1,15 @@
 import { useLanguage } from "../internationalization/LanguageContext.jsx";
 
 export default function Help() {
-  const { languageData } = useLanguage();
+  const { languageData, currentLanguage } = useLanguage();
 
   return (
     <section className={"help-section"}>
-      <h1 className={"uppercase"}>{languageData.help.title}</h1>
+      <h1
+        className={`uppercase ${currentLanguage === "pl" ? "polish-text" : ""}`}
+      >
+        {languageData.help.title}
+      </h1>
       <p>{languageData.help.description}</p>
     </section>
   );
